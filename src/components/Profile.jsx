15e6302d3b6d3ba0b28906b642fcdecc9cdd265e
Zhekas525 <hawewow@mail.ru>
2022-08-14 22:37:22 +0700

    some changes

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+  return ( <div><div className='content'>
+  <img src='https://cs10.pikabu.ru/post_img/big/2019/08/29/11/1567106726159459734.jpg' />
+</div>
+<div>
+  ava + descriptoin
+  <div>
+    My posts
+    <div>
+      new post
+    </div>
+    <div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+      </div>
+      </div>)
+}
+export default Profile;
